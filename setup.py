6f9ebad11fa4a946ecad390214af86a6515554bf
Matthew R Becker <becker.mr@gmail.com>
2014-10-04 01:35:37 -0700

    added files

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+import glob
+from distutils.core import setup
+
+setup(name="mcmc", 
+      version="0.1.0",
+      description="biggles code to plot mcmc info",
+      license = "GPL",
+      author="Erin Scott Sheldon",
+      author_email="erin.sheldon@gmail.com",
+      packages=['mcmc'])
+
